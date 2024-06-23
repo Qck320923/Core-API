a8b107fbd351e63a3a20400acb8b661df8c55652
L.W.Kevin0wvf <152294811+Qck320923@users.noreply.github.com>
2024-06-23 17:17:30 +0800

    Update server.index.js

--- extension API/carbonCoreBase/1.0.0/server.index.js
+++ extension API/carbonCoreBase/1.0.0/server.index.js
@@ -26,7 +26,7 @@ class CarbonCoreBase {
          */
         compare(v1, v2, cmp) {
             var vA = v1.split("."), vB = v2.split("."), len = Math.min(vA.length, vB.length);
-            for (var i = 0; i < len; i++) if (cmp(vA[i], vB[i])) return true;
+            for (var i = 0; i < len; i++) if (cmp(Number(vA[i]), Number(vB[i]))) return true;
             return false;
         }
     }
@@ -37,3 +37,6 @@ class CarbonCoreBase {
 const carbonCoreBase = new CarbonCoreBase();
 globalThis.carbonCoreBase = carbonCoreBase;
 //在这行注释后面可以调用carbonCoreBase...
+
+
+
